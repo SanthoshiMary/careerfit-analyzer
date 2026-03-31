4bc50e2fbd335219fccfdf1694040785c6338b9c
SanthoshiMary <santhoshimary07@gmail.com>
2026-03-31 22:26:57 +0530

    Delete Resume_skill_Match_analyzer directory

--- Resume_skill_Match_analyzer/static/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const fadeElements = document.querySelectorAll(".fade-up");
-
-    fadeElements.forEach((el, index) => {
-        el.style.animationDelay = `${index * 0.15}s`;
-    });
-});
